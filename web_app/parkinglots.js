00a2b66e235e65e0fd96c80cfbd652810fca22a4
Jan Lippert <lippertsjan@gmail.com>
2014-12-14 19:14:50 +0100

    Added basic web app

---
+++ web_app/parkinglots.js
@@ -0,0 +1,81 @@
+var lots = 
+{
+    "P1": {
+        "name": "Königsplatz",
+        "lat": 51.71694575625038,
+        "lon": 8.747949600219725,
+        "url": "http://www.paderborn.de/microsite/asp/parken_in_der_city/TG_Koenigsplatz.php"
+    },
+    "P2": {
+        "name": "Florianstraße",
+        "lat": 51.71694575625038,
+        "lon": 8.747949600219725,
+        "url": "http://www.paderborn.de/microsite/asp/parken_in_der_city/PP_Florianstrasse.php"
+    },
+    "P3": {
+        "name": "Neuhäuser Tor",
+        "lat": 51.72079421539546,
+        "lon": 8.747305870056152,
+        "url": "http://www.paderborn.de/microsite/asp/parken_in_der_city/PH_Neuhaeuser_Tor.php"
+    },
+    "P4": {
+        "name": "Paderhalle/Maspernplatz",
+        "lat": 51.722990814236,
+        "lon": 8.754022121429443,
+        "url": "http://www.paderborn.de/microsite/asp/parken_in_der_city/PP_Paderhalle.php"
+    },
+    "P5": {
+        "name": "Rolandsweg",
+        "lat": 51.727363755321555,
+        "lon": 8.754692673683165,
+        "url": "http://www.paderborn.de/microsite/asp/parken_in_der_city/PH_Rolandsweg.php"
+    },
+    "P6": {
+        "name": "Libori-Galerie",
+        "lat": 51.71629766743732,
+        "lon": 8.757686018943787,
+        "url": "http://www.paderborn.de/microsite/asp/parken_in_der_city/PH_Liborigalerie.php"
+    },
+    "P7": {
+        "name": "Le-Mans-Wall/Liboriberg",
+        "lat": 51.714978196381416,
+        "lon": 8.753067255020142,
+        "url": "http://www.paderborn.de/microsite/asp/parken_in_der_city/PP_Liboriberg.php"
+    },
+    "P8": {
+        "name": "Volksbank/Theater",
+        "lat": 51.71776333196541,
+        "lon": 8.756720423698425,
+        "url": "http://www.paderborn.de/microsite/asp/parken_in_der_city/Tiefgarage_Volksbank.php"
+    },
+    "P9": {
+        "name": "Domplatz",
+        "lat": 51.71879691262835,
+        "lon": 8.756704330444334,
+        "url": "http://www.paderborn.de/microsite/asp/parken_in_der_city/PP_Domplatz.php"
+    },
+    "P10": {
+        "name": "Westernmauer",
+        "lat": 51.71744095631442,
+        "lon": 8.747268319129944,
+        "url": "http://www.paderborn.de/microsite/asp/parken_in_der_city/PP_Westernmauer.php"
+    },
+    "P11": {
+        "name": "Am Westerntor",
+        "lat": 51.716872639002936,
+        "lon": 8.746179342269897,
+        "url": "http://www.paderborn.de/microsite/asp/parken_in_der_city/PP_Westerntor.php"
+    },
+    "P12": {
+        "name": "Rathenaustraße",
+        "lat": 51.713967794533986,
+        "lon": 8.737274408340454,
+        "url": "http://www.paderborn.de/microsite/asp/parken_in_der_city/Parkplatz_Rathenaustrasse.php"
+    },
+    "P13": {
+        "name": "Hauptbahnhof",
+        "lat": 51.71270476048044,
+        "lon": 8.738036155700684,
+        "url": "http://www.paderborn.de/microsite/asp/parken_in_der_city/parkplatz_Hauptbahnhof.php"
+    }
+}
