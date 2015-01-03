85c4fef10cab6c17c1826d02a55e38f3ce578859
Jan Lippert <ljan@mail.upb.de>
2015-01-03 19:43:24 +0100

    Better csv log

--- parking_crawler/crawl.sh
+++ parking_crawler/crawl.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-rm newest.json
-scrapy crawl pspider -o newest.json -t json
-cp newest.json ../web_app/
+COMPLETE_LOG_FILE=parking_log.csv
+if [ ! -f $COMPLETE_LOG_FILE ]; then
+	touch $COMPLETE_LOG_FILE
+	echo '_id,name,count,free,timestamp,lat,lon,url' >> $COMPLETE_LOG_FILE
+fi
+
+crawlDate=$(date +%s)
+scrapy crawl pspider -o $crawlDate.json -t json
+tail -n +2 pspider_log.csv >> $COMPLETE_LOG_FILE
+mv $crawlDate.json ../web_app/newest.json
--- parking_crawler/crawlAndLog.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-crawlDate=$(date +%s)
-scrapy crawl pspider -o $crawlDate.json -t json
-cp $crawlDate.json newest.json
-cp newest.json ../web_app/
